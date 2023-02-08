7a56dfc4e6a167b9fe5b1d65c3ce5e55509a5c63
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2023-02-08 15:00:02 +0900

    Add:커스텀 pipe로 업데이트 문구 예외처리

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,10 +1,29 @@
 import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common/exceptions';
+import { BoardStatus } from '../board.model';
+
+// export class BoardStatusValidationPipe implements PipeTransform {
+//   transform(value: any, metadata: ArgumentMetadata) {
+//     console.log('value', value);
+//     console.log('metadata', metadata);
+
+//     return value;
+//   }
+// }
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log('value', value);
-    console.log('metadata', metadata);
+  private isStatisValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
 
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatisValid(value)) {
+      throw new BadRequestException(`${value} is not in the status options`);
+    }
     return value;
   }
 }
